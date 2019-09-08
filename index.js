411d2be9ec02f44c3c1f8af32ed0e03d2cbc71ff
Robert Shaw <r.shaw1@yahoo.com>
2019-09-08 02:54:43 -0500

    image click count: basic implementation

---
+++ index.js
@@ -0,0 +1,30 @@
+
+class BeautifulImg {
+  constructor(imgElement, i) {
+    this.img = imgElement;
+    this.img.id = this.img.alt || `img${i}`;
+    this.clickCount = 0;
+
+    this.img.onclick = this.incrementClickCount;
+  }
+
+  incrementClickCount = () => {
+    this.clickCount++
+    this.img.nextElementSibling.textContent = this.clickCount;
+  }
+
+}
+
+const beautifulImgList = [];
+
+function configureImages(imgCollection) {
+  for (let i = 0; i < imgCollection.length; i++) {
+    const img = imgCollection[i];
+    beautifulImgList.push(new BeautifulImg(img, i));
+  }
+}
+
+window.onload = event => {
+  const imgCollection = document.querySelectorAll('.img-clk');
+  configureImages(imgCollection);
+}
